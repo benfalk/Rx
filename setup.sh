299a5592fe2539149e43d01da14900f1df4f9d3a
Benjamin Falk <bn_falk@yahoo.com>
2015-08-21 20:11:35 -0500

    clipboard manager is nice, hopefully I can plug it into mutate

--- setup.sh
+++ setup.sh
@@ -18,6 +18,7 @@ sudo apt-get install phantomjs -y
 sudo apt-get install htop -y
 sudo apt-get install multitail -y
 sudo apt-get install parallel -y
+sudo apt-get install diodon -y
 
 # Latest Nodejs
 sudo add-apt-repository ppa:chris-lea/node.js -y
